a2686ede73a7eecf50c33e2deaf983f6036a372b
v-b-a <valiakhmedov.ba@gmail.com>
2022-11-11 08:31:04 +0300

    edit MigrationGenerator

--- app/src/main/java/hexlet/code/MigrationGenerator.java
+++ app/src/main/java/hexlet/code/MigrationGenerator.java
@@ -10,8 +10,8 @@ public class MigrationGenerator {
         // Создаём миграцию
         DbMigration dbMigration = DbMigration.create();
         // Указываем платформу, в нашем случае H2
-//        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
         dbMigration.addPlatform(Platform.H2, "h2");
+        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
         // Генерируем миграцию
         dbMigration.generateMigration();
     }
