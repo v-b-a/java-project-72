a4f744afb3169edc3fb70a3aa37e69cac8fd84a0
v-b-a <valiakhmedov.ba@gmail.com>
2022-11-29 07:52:40 +0300

    refactor 29.11 - 3

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -20,16 +20,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class AppTest {
     private final int responseCode200 = 200;
     private final int responseCode302 = 302;
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url url;
+    private static Database database;
+
 
     @Test
     void testInit() {
         assertThat(true).isEqualTo(true);
     }
-
-    private static Javalin app;
-    private static String baseUrl;
-    private static Url url;
-    private static Database database;
 
     @BeforeAll
     public static void beforeAll() {
@@ -47,12 +47,15 @@ public final class AppTest {
 
     @BeforeEach
     void beforeEach() {
-        database.beginTransaction();
+//        database.beginTransaction();
     }
 
     @AfterEach
     void afterEach() {
-        database.rollbackTransaction();
+//        database.rollbackTransaction();
+        int url1 = new QUrl()
+                .id.between(1, Integer.MAX_VALUE)
+                .delete();
     }
 
     @Test
@@ -61,113 +64,111 @@ public final class AppTest {
         assertThat(port).isNotNull();
     }
 
-    @Nested
-    class UrlTest {
-        @Test
-        void testIndex() {
-            HttpResponse<String> response = Unirest.get(baseUrl).asString();
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            assertThat(response.getBody()).contains("Анализатор web-сайтов");
-        }
-
-        @Test
-        void testUrlsList() {
-            HttpResponse<String> response = Unirest
-                    .get(baseUrl + "/urls")
-                    .asString();
-            String body = response.getBody();
-
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            assertThat(body).contains("Список проверок");
-        }
-
-        @Test
-        void testCreate() {
-            String inputUrl = "https://www.google.ru/webhp?authuser=1";
-            String normalizedUrl = "https://www.google.ru";
-            HttpResponse responsePost = Unirest
-                    .post(baseUrl + "/urls")
-                    .field("url", inputUrl)
-                    .asEmpty();
-
-            assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
-//            assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
-
-            HttpResponse<String> response = Unirest
-                    .get(baseUrl + "/urls")
-                    .asString();
-            String body = response.getBody();
-
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            Url dbUrl = new QUrl()
-                    .name.equalTo(normalizedUrl)
-                    .findOne();
-            assertThat(dbUrl).isNotNull();
-            assertThat(dbUrl.getName()).isEqualTo(normalizedUrl);
-            assertThat(body).containsIgnoringCase("Страница успешно добавлена");
-        }
-
-        @Test
-        void testIncorrectUrl() {
-            String incorrectValue1 = "tree";
-            HttpResponse responsePost = Unirest
-                    .post(baseUrl + "/urls")
-                    .field("url", incorrectValue1)
-                    .asEmpty();
-
-            assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
-            HttpResponse<String> response = Unirest
-                    .get(baseUrl)
-                    .asString();
-            String body = response.getBody();
-
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            assertThat(body).contains("Некорректный URL");
-        }
-
-        @Test
-        void testNullUrl() {
-            String incorrectValue1 = null;
-            HttpResponse responsePost = Unirest
-                    .post(baseUrl + "/urls")
-                    .field("url", incorrectValue1)
-                    .asEmpty();
-
-            assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
-            HttpResponse<String> response = Unirest
-                    .get(baseUrl)
-                    .asString();
-            String body = response.getBody();
-
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            assertThat(body).contains("Некорректный URL");
-        }
-
-        @Test
-        void repeatUrl() {
-            String correctValue = "https://www.google.ru/webhp?authuser=1";
-            HttpResponse responsePost = Unirest
-                    .post(baseUrl + "/urls")
-                    .field("url", correctValue)
-                    .asEmpty();
-
-            assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
-
-            HttpResponse repeatPost = Unirest
-                    .post(baseUrl + "/urls")
-                    .field("url", correctValue)
-                    .asEmpty();
-
-            assertThat(repeatPost.getStatus()).isEqualTo(responseCode302);
-
-            HttpResponse<String> response = Unirest
-                    .get(baseUrl)
-                    .asString();
-            String body = response.getBody();
-
-            assertThat(response.getStatus()).isEqualTo(responseCode200);
-            assertThat(body).contains("Этот сайт уже существует");
-        }
+    @Test
+    void testIndex() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        assertThat(response.getBody()).contains("Анализатор web-сайтов");
     }
+
+    @Test
+    void testUrlsList() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        assertThat(body).contains("Список проверок");
+    }
+
+    @Test
+    void testCreate() {
+        String inputUrl = "https://www.google.ru/webhp?authuser=1";
+        String normalizedUrl = "https://www.google.ru";
+        HttpResponse responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", inputUrl)
+                .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
+        assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        Url dbUrl = new QUrl()
+                .name.equalTo(normalizedUrl)
+                .findOne();
+        assertThat(dbUrl).isNotNull();
+        assertThat(dbUrl.getName()).isEqualTo(normalizedUrl);
+        assertThat(body).containsIgnoringCase("Страница успешно добавлена");
+    }
+
+    @Test
+    void testIncorrectUrl() {
+        String incorrectValue1 = "tree";
+        HttpResponse responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", incorrectValue1)
+                .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
+        HttpResponse<String> response = Unirest
+                .get(baseUrl)
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        assertThat(body).contains("Некорректный URL");
+    }
+
+    @Test
+    void testNullUrl() {
+        String incorrectValue1 = null;
+        HttpResponse responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", incorrectValue1)
+                .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
+        HttpResponse<String> response = Unirest
+                .get(baseUrl)
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        assertThat(body).contains("Некорректный URL");
+    }
+
+    @Test
+    void repeatUrl() {
+        String correctValue = "https://www.google.ru/webhp?authuser=1";
+        HttpResponse responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", correctValue)
+                .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(responseCode302);
+
+        HttpResponse repeatPost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", correctValue)
+                .asEmpty();
+
+        assertThat(repeatPost.getStatus()).isEqualTo(responseCode302);
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl)
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(responseCode200);
+        assertThat(body).contains("Этот сайт уже существует");
+    }
+//    }
 
 }
