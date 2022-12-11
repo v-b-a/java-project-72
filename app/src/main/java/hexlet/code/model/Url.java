9e7ddf43e2796fd198818ad1ffa726efaa6929d3
v-b-a <valiakhmedov.ba@gmail.com>
2022-12-11 10:33:53 +0300

    refactor 11.12 - 2

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -5,7 +5,12 @@ import io.ebean.annotation.WhenCreated;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 import java.time.Instant;
 import java.util.List;
 
