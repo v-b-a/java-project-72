ee03ca085628cddc8fabdd6de05cf4c3dfa62fa3
v-b-a <valiakhmedov.ba@gmail.com>
2022-12-28 14:30:52 +0300

    refactor 28.12 - 1

---
+++ app/src/main/java/hexlet/code/UrlDto.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import hexlet.code.model.UrlCheck;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Setter
+@Getter
+public class UrlDto {
+    private long id;
+    private String name;
+    private Instant createdAt;
+    private List<UrlCheck> urlCheckList;
+
+    public Instant getLastCheckDate() {
+        if (!urlCheckList.isEmpty()) {
+            return urlCheckList.get(urlCheckList.size() - 1).getCreatedAt();
+        }
+        return null;
+    }
+    public Integer getLastCheckStatus() {
+        if (!urlCheckList.isEmpty()) {
+            return urlCheckList.get(urlCheckList.size() - 1).getStatusCode();
+        }
+        return null;
+    }
+}
